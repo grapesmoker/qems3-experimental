3a3fbb1678b86d9d86846508313bc1636a520d90
Jerry Vinokurov <grapesmoker@gmail.com>
2018-12-15 21:10:13 -0500

    namechange

--- manage.py
+++ manage.py
@@ -3,7 +3,7 @@ import os
 import sys
 
 if __name__ == '__main__':
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qems3_new.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qems3.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- qems3/wsgi.py
+++ qems3/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for qems3_new project.
+WSGI config for qems3 project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qems3_new.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'qems3.settings')
 
 application = get_wsgi_application()
