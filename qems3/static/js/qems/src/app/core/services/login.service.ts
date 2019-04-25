076b110db5b379b465b723f1f145af25f669908f
Jerry Vinokurov <grapesmoker@gmail.com>
2019-04-24 23:54:05 -0400

    reqs

---
+++ qems3/static/js/qems/src/app/core/services/login.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class LoginService {
+
+  API_URL = 'http://localhost:8000';
+
+  constructor(private httpClient: HttpClient) { }
+
+  login(username: string, password: string) {
+    return this.httpClient.post<any>(`${this.API_URL}/rest-auth/login`, { username, password})
+    .pipe(map(user => {
+      console.log(user);
+
+      return user;
+    }))
+  }
+}
