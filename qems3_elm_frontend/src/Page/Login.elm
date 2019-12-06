0be534d06de023ffb328b74fac48fd0e004d2c0e
Jerry Vinokurov <grapesmoker@gmail.com>
2019-12-05 20:47:52 -0500

    refactor

--- qems3_elm_frontend/src/Page/Login.elm
+++ qems3_elm_frontend/src/Page/Login.elm
@@ -4,6 +4,9 @@ import Browser.Navigation as Nav
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Html.Events exposing (onClick, onSubmit, onInput)
+import Http
+import Json.Encode as Encode exposing (..)
+import Json.Decode as Decode exposing (Decoder, list, string)
 
 
 type Msg
@@ -12,13 +15,28 @@ type Msg
     | SetUsername String
     | SetPassword String
     | SubmitLogIn
+    | AuthResponseMsg (Result Http.Error String)
 
-type alias Username = String
-type alias Password = String
-      
+type alias Model =
+    { username : String
+    , password : String
+    }
+    
 type LogInFormField
     = Username
     | Password
+
+
+encodeCreds : Model -> Encode.Value
+encodeCreds model =
+    Encode.object [ ("username", Encode.string model.username)
+                  , ("password", Encode.string model.password)
+                  ]
+
+ 
+tokenDecoder : Decoder String
+tokenDecoder =
+    Decode.field "token" Decode.string
 
       
 loginPageView : Html Msg
@@ -45,3 +63,23 @@ loginPageView =
          , button [ class "btn btn-primary" ] [ text "Login" ]
          ]
        ]
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        SetUsername username ->
+            ( { model | username = username }, Cmd.none )
+        SetPassword password ->
+            ( { model | password = password }, Cmd.none )
+        SubmitLogIn ->
+            let
+                body = model |> encodeCreds |> Http.jsonBody
+            in
+                ( model, Http.post
+                      { url = "qsub/webapp_login/"
+                      , body = body
+                      , expect = Http.expectJson AuthResponseMsg tokenDecoder
+                      } )
+        _ ->
+            ( model, Cmd.none)
